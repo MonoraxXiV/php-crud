18ff5dbb635e00df794df03f205cd73e431bc605
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-11-02 16:25:28 +0100

    Form for teachers now show

--- Controller/TeacherController.php
+++ Controller/TeacherController.php
@@ -3,15 +3,20 @@
 
 class TeacherController
 {
+    /*
     private string $teacherNameError;
     private string $teacherEmailError;
     private string $teacherClassError;
     private string $teacher_Class;
-
-    public function render(array $POST, array $GET)
+*/
+    public function render()
     {
-        $connect=new Connection();
+
+        $connect = new Connection();
         $connect->openConnection();
+        $Teachers = $connect->displayTeacher();
+        $form = "";
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (empty($_POST['name'])) {
                 $this->teacherNameError = "name is required" . '<br>';
@@ -31,14 +36,16 @@ class TeacherController
                 $teacher_class = $_POST['ClassName'];
             }
 
+            /*
             if ($this->teacherNameError== "" && $this->teacherEmailError=="" && $this->teacherClassError=""){
                 $teacher=new Teacher($teacher_name,$teacher_email,$teacher_class);
                 $connect->insertTeacher($teacher);
 
             }
+            */
 
 
-            require 'View/TeacherView.php';
         }
+        require 'View/TeacherView.php';
     }
 }
