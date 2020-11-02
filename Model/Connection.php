2f40ba04c982638ac1157a2b5b5799cc0286e7d1
Michiel Coulier <michielcoulier@gmail.com>
2020-11-02 11:56:51 +0100

    adding connection class

--- Model/Connection.php
+++ Model/Connection.php
@@ -1,7 +1,24 @@
 <?php
 
-
 class Connection
 {
+    private $PDO;
 
+    public function openConnection(): PDO
+    {
+
+        require('resources/config.php');
+
+        $driverOptions = [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        $pdo = new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
+
+        $this->PDO = $pdo;
+        return $pdo;
+
+    }
 }
