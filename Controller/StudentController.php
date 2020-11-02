0e96eb3c2bd21d73f7fddf8970b23814a6f5b674
Michiel Coulier <michielcoulier@gmail.com>
2020-11-02 15:57:04 +0100

    able to show student data on page

--- Controller/StudentController.php
+++ Controller/StudentController.php
@@ -1,7 +1,36 @@
 <?php
+declare(strict_types = 1);
 
 
 class StudentController
 {
+    public function render()
+    {
+        $connection = new Connection();
+        $showStudents = $connection->displayStudent();
 
+        $sName = $sClass = $sEmail = $form = "";
+        $sNameError = $sClassError = $sEmailError = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+            if (empty($_POST["student_name"])) {
+                $sNameError = "* Student name required!";
+            } else {
+                $sName = $_POST["student_name"];
+            }
+
+            if (empty($_POST["student_email"])) {
+                $sEmailError = "* Student email required!";
+            } else {
+                $sEmail = $_POST["student_email"];
+            }
+
+/*            $students = new Student($sName, $sEmail);*/
+/*            $connection->insertStudent($students);*/
+
+        }
+
+        require "View/StudentView.php";
+    }
 }
