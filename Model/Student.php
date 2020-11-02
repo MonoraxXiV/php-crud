3338064d2518db52f397020fecb37ef3a01dc6f2
MZ <nikita.zholkovskyi@gmail.com>
2020-11-02 12:16:40 +0100

    added student class

--- Model/Student.php
+++ Model/Student.php
@@ -3,5 +3,59 @@
 
 class Student
 {
+    private int $student_id;
+    private string $student_name;
+    private string $student_email;
+    private int $student_class;
+
+    /**
+     * Student constructor.
+     * @param int $id
+     * @param string $first_name
+     * @param string $last_name
+     * @param string $email
+     * @param $created_at
+     */
+    public function __construct(int $student_id, string $student_name, string $student_email, int $student_class)
+    {
+        $this->student_id = $student_id;
+        $this->student_name = $student_name;
+        $this->student_email = $student_email;
+        $this->student_class = $student_class;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStudentId(): int
+    {
+        return $this->student_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStudentName(): string
+    {
+        return $this->student_name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStudentEmail(): string
+    {
+        return $this->student_email;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStudentClass(): int
+    {
+        return $this->student_class;
+    }
+
 
 }
+
