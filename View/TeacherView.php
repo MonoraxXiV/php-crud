ca751ee53e05936b8fc4332b98f5c57510c8ea5d
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-11-02 15:41:41 +0100

    adding start of teacher, removed id from constructor

---
+++ View/TeacherView.php
@@ -0,0 +1,34 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Teacher registration</title>
+</head>
+<body>
+
+<form  method="post">
+    <h1>Registration form</h1>
+    <label for="name">Name:</label><br>
+    <input type="text" id="name" name="name" ><br>
+
+    <label for="email">e-mail:</label><br>
+    <input type="text" id="email" name="email" value=""><br>
+
+    <label for="ClassName">Class:</label><br>
+    <input type="text" id="ClassName" name="ClassName" value=""><br>
+
+    <input type="submit" value="Submit">
+
+
+
+
+
+
+</form>
+</body>
+</html>
